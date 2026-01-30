97dcbc78ccd3ce2f56870ffc33e9c39cc3646a70
smghulamghausfaiyaz <smghulamghaus.faiyaz@intglobal.com>
2026-01-30 17:12:08 +0530

    feat: added cors to express server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,32 @@
 import express from "express";
 import * as color from "colors";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
+const allowOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+
+const corsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow: boolean) => void,
+  ) => {
+    if (allowOrigins.indexOf(origin!) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"), false);
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
 
 app.listen(3030, (err) => {
   if (err) {
